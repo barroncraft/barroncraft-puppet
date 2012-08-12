fc70478021628635d8d28a90557fac5888c27890
Nullreff <rsmendivil@gmail.com>
2012-08-11 19:58:34 -0800

    Moved all configuration into setup.pp

--- manifests/setup.pp
+++ manifests/setup.pp
@@ -1,5 +1,76 @@
-$commonPack = [ "sudo", "screen", "puppet", "vim", "git" ]
-$minecraftPack = [ "openjdk-6-jre" ]
+#################
+# Configuration #
+#################
 
-package { commonPack:    ensure => installed }
-package { minecraftPack: ensure => installed }
+# Add this host as a subdoman
+$serverDomain = "barroncraft.com"
+$serverDir    = "/home/minecraft"
+
+#################
+# Common Config #
+#################
+
+$commonPack = [ "sudo", "screen", "puppet", "vim", "git", "figlet", "wget", "less", "tail" ]
+package { commonPack: ensure => installed }
+
+host { "self":
+    ensure       => present,
+    name         => $fqdn,
+    host_aliases => [ $hostname, "${hostname}.${serverDomain}" ],
+    ip           => $ipaddress,
+}
+
+file { "motd":
+    ensure  => file,
+    path    => "/etc/motd",
+    mode    => "0644",
+    content => "Minecraft node ${hostname}",
+}
+
+####################
+# Minecraft Config #
+####################
+
+## Packages ##
+
+$minecraftPack = [ "openjdk-6-jre" ]
+package { minecraftPack: 
+    ensure => installed 
+}
+
+## Init Script ##
+
+file { "minecraftInit":
+    path   => "/etc/init.d/minecraft"
+    ensure => link,
+    target => "${serverDir}/bin/minecraft.sh"
+}
+
+## Direcories ##
+$minecraftDirs = [ 
+    "${serverDir}"
+    "${serverDir}/backups", 
+    "${serverDir}/bin", 
+    "${serverDir}/server", 
+    "${serverDir}/logs", 
+    "${serverDir}/worlds" ]
+
+file { $minecraftDirs: 
+    ensure  => "directory",
+    owner   => "minecraft",
+    group   => "mc-editors",
+    mode    => 774,
+}
+
+## Users & Groups ##
+
+user { "minecraftUser":
+    name => "minecraft",
+    ensure => "present",
+    home => "/home/minecraft"
+}
+
+group { "minecraftEditors":
+    name   => "mc-editors",
+    ensure => "present",
+}

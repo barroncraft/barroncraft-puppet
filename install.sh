d36d98e3894fc4fc5895369c249dab1b252e7e4a
Nullreff <rsmendivil@gmail.com>
2012-10-14 01:26:15 -0800

    Fixed prompts in install script

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 apt-get update
-apt-get upgrade
-apt-get install puppet git-core
+apt-get upgrade -y
+apt-get install -y puppet git-core
 rm -rf /etc/puppet/*
 git clone git://github.com/Nullreff/minecraft-puppet.git /etc/puppet
-puppet apply /etc/puppet/manifests/setup.pp
+puppet /etc/puppet/manifests/setup.pp

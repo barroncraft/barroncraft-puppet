f40b8e9456b3f36ae9834174b6594d68f3b7eb64
Nullreff <rsmendivil@gmail.com>
2012-10-17 17:58:33 -0800

    Renamed setup.pp to baseSetup.pp

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ apt-get upgrade -y
 apt-get install -y puppet git-core
 rm -rf /etc/puppet/*
 git clone git://github.com/Nullreff/minecraft-puppet.git /etc/puppet
-puppet /etc/puppet/manifests/setup.pp
+puppet /etc/puppet/manifests/baseSetup.pp

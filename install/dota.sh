b19ccc15525616a6905b3f4593c14b8fdb4734ef
Ryan Mendivil <rsmendivil@gmail.com>
2013-01-13 16:07:08 -0900

    Change repository location to barroncraft

--- install/dota.sh
+++ install/dota.sh
@@ -4,5 +4,5 @@ apt-get update
 apt-get upgrade -y
 apt-get install -y puppet git-core
 rm -rf /etc/puppet/*
-git clone git://github.com/Nullreff/minecraft-puppet.git /etc/puppet
+git clone git://github.com/barroncraft/barroncraft-puppet.git /etc/puppet
 puppet /etc/puppet/manifests/dotaSetup.pp

fa1304153740fea81c503dfce770efc0d154291f
Ryan Mendivil <rsmendivil@gmail.com>
2013-02-04 20:57:11 -0900

    Force the remove of hidden files in the puppet folder

--- install/dota.sh
+++ install/dota.sh
@@ -3,6 +3,6 @@
 apt-get update
 apt-get upgrade -y
 apt-get install -y puppet git-core
-rm -rf /etc/puppet/*
+rm -rf /etc/puppet; mkdir /etc/puppet
 git clone git://github.com/barroncraft/barroncraft-puppet.git /etc/puppet
 puppet /etc/puppet/manifests/dotaSetup.pp

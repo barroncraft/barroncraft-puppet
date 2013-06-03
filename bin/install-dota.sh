d387c61ce868af4ab0550e1ca873d4816427eaa4
Ryan Mendivil <rsmendivil@gmail.com>
2013-06-02 22:12:10 -0800

    Add RPM Forge source so htop can be installed

--- bin/install-dota.sh
+++ bin/install-dota.sh
@@ -6,6 +6,7 @@ if [ -f /etc/debian_version ]; then
     apt-get upgrade -y
     apt-get install -y puppet git-core
 elif [ -f /etc/redhat-release ]; then
+    ARCH=`uname -i`
     if grep ' 5\.' /etc/redhat-release >/dev/null; then
         RELEASE='5'
     elif grep ' 6\.' /etc/redhat-release >/dev/null; then
@@ -15,7 +16,15 @@ elif [ -f /etc/redhat-release ]; then
         exit 1
     fi
     echo "Detected CentOS/RedHat system, installing..."
+
+    # Add RPM Forge source
+    rpm --import http://apt.sw.be/RPM-GPG-KEY.dag.txt
+    rpm -i http://packages.sw.be/rpmforge-release/rpmforge-release-0.5.2-2.el$RELEASE.rf.$ARCH.rpm
+
+    # Add Puppet source
     rpm -i http://yum.puppetlabs.com/el/$RELEASE/products/i386/puppetlabs-release-$RELEASE-6.noarch.rpm
+
+    # Update all packages and install puppet and git
     yum update -y
     yum upgrade -y
     yum install -y puppet git

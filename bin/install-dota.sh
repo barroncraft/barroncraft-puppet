84cea120eafd894b09612134617b2f2580dfe903
Ryan Mendivil <rsmendivil@gmail.com>
2013-06-02 23:07:52 -0800

    Add checks to prevent installation on older operating systems

--- bin/install-dota.sh
+++ bin/install-dota.sh
@@ -1,28 +1,28 @@
 #!/bin/sh
 
 if [ -f /etc/debian_version ]; then
+    if ! grep '7\.' /etc/debian_version >/dev/null; then
+        echo "Unsuported Debian version, please use 7.x."
+        exit 1
+    fi
     echo "Detected Debian system, installing..."
     apt-get update
     apt-get upgrade -y
-    apt-get install -y puppet git-core
+    apt-get install -y puppet git
+
 elif [ -f /etc/redhat-release ]; then
-    ARCH=`uname -i`
-    if grep ' 5\.' /etc/redhat-release >/dev/null; then
-        RELEASE='5'
-    elif grep ' 6\.' /etc/redhat-release >/dev/null; then
-        RELEASE='6'
-    else
-        echo "Unsuported CentOS/RedHat version, please use 5.x or 6.x."
+    if ! grep ' 6\.' /etc/redhat-release >/dev/null; then
+        echo "Unsuported CentOS/RedHat version, please use 6.x."
         exit 1
     fi
     echo "Detected CentOS/RedHat system, installing..."
 
     # Add RPM Forge source
     rpm --import http://apt.sw.be/RPM-GPG-KEY.dag.txt
-    rpm -i http://packages.sw.be/rpmforge-release/rpmforge-release-0.5.2-2.el$RELEASE.rf.$ARCH.rpm
+    rpm -i http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.`uname -i`.rpm
 
     # Add Puppet source
-    rpm -i http://yum.puppetlabs.com/el/$RELEASE/products/i386/puppetlabs-release-$RELEASE-6.noarch.rpm
+    rpm -i http://yum.puppetlabs.com/el/6/products/i386/puppetlabs-release-6-6.noarch.rpm
 
     # Update all packages and install puppet and git
     yum update -y
@@ -30,6 +30,7 @@ elif [ -f /etc/redhat-release ]; then
     yum install -y puppet git
 else
     echo 'Unsuported operating system.  Email contact@barroncraft.com if you need help.'
+    exit 1
 fi
 
 rm -rf /etc/puppet; mkdir /etc/puppet

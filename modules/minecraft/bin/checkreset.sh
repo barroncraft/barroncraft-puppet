41374effe0e2b85da4e713a26efe1bcc41fc29a2
Ryan Mendivil <rsmendivil@gmail.com>
2013-06-03 17:14:16 -0800

    Add reset safety measure and documentation to reset check script

--- modules/minecraft/bin/checkreset.sh
+++ modules/minecraft/bin/checkreset.sh
@@ -1,7 +1,17 @@
 MCDIR=/home/minecraft
 RESETFILE=$MCDIR/server/reset-required
+STARTFILE=$MCDIR/server/auto-start
+MCSCRIPT=$MCDIR/bin/minecraft.sh
 
+# If the server writes out a 'reset-required' file, reset the server
 if [ -e $RESETFILE ]; then
-    $MCDIR/bin/minecraft.sh reset
+    touch $STARTFILE
     rm -f $RESETFILE
+    $MCSCRIPT reset
+
+# There have been issues with the server sometimes not coming back online
+# after a reset.  This is a safty measure to make sure that an error during
+# the reset won't prevent it from coming back online.
+elif [ -e $STARTFILE ]; then
+    $MCSCRIPT start
 fi
